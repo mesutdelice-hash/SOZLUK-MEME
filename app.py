e0b8f35cd2ce0893d0a930104f4d0f46274e62c2
mesutdelice-hash <mesutdelice@gmail.com>
2026-09-18 21:12:03 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "ROFL ": "bir şakaya karşılık cevap"
+            }
+
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    # Kelime eşleşiyorsa ne yapmalıyız?
+    print("anlamı",meme_dict[word])
+else:
+    # Kelime eşleşmiyorsa ne yapmalıyız?
+    print("bu kelime eşleşmiyor")
+
+    
